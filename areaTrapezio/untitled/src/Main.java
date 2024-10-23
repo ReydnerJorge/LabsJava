9dc5e3bc688ee649d7b7663ef08c303dbd970514
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2024-10-23 17:13:27 -0300

    update labs

---
+++ areaTrapezio/untitled/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+    double area, h, b, B;
+
+    h = 5.0;
+    b = 6.0;
+    B = 8.0;
+
+    area = (b + B) / 2.0 * h;
+
+    System.out.println("Área do Trapezio é: " + area);
+
+    double a;
+    int e;
+
+    a = 5.0;
+    e = (int) a; //casting
+
+    System.out.println(e);
+    }
+}
