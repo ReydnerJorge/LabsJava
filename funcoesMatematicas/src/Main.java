9c05baa851b8cb750f9c3cb9fd83887387d9ee57
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2024-10-29 15:43:05 -0300

    Update main funções matemáticas

--- funcoesMatematicas/src/Main.java
+++ funcoesMatematicas/src/Main.java
@@ -11,5 +11,17 @@ public class Main {
         System.out.println("A Raiz quadrada de " + x + " = " + A);
         System.out.println("A Raiz quadrada de " + y + " = " + B);
         System.out.println("A Raiz quadrada de 25 = " + C);
+
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+
+        A = Math.abs(y);
+        B = Math.abs(z);
+        System.out.println("O valor absoluto de " + y + " = " + A);
+        System.out.println("O valor absoluto de " + z + " = " + B);
     }
 }
